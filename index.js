958608d8479b91d796c4a9e017f400752ee6a7bc
Afsara Silvi Ami <afsarasilvi44@gmail.com>
2026-05-19 05:18:04 +0600

    did the add facility

--- index.js
+++ index.js
@@ -73,7 +73,15 @@ async function run() {
       res.send(result)
     })
     //deleting facility
-  
+     app.delete("/facilities/:id", async(req, res)=>{
+      const {id} = req.params
+      const result = await facilityCollection.deleteOne({_id: new ObjectId(id)})
+      res.send(result)
+
+     })
+
+     //update facility
+     
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
